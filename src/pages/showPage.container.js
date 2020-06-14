4987ec9f8e3e99e6032a2145fb8a2f44d3970702
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-06-14 15:08:38 +0100

    add loader to show page

--- src/pages/showPage.container.js
+++ src/pages/showPage.container.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import ShowPage from './showPage';
+import Loader from '../components/Loader';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '20px',
+  },
+}));
 
 const ShowPageContainer = ({ match }) => {
+  const classes = useStyles();
   const [show, setShow] = useState(null);
   const [notFound, setNotFound] = useState(false);
+  const [Loading, setLoading] = useState(<Loader />);
 
   useEffect(() => {
     axios
@@ -15,8 +27,12 @@ const ShowPageContainer = ({ match }) => {
     return () => '';
   }, []);
 
-  const loading = notFound ? <p>Show not found</p> : <p>Loading ...</p>;
-  return <div>{show ? <ShowPage show={show} /> : loading}</div>;
+  useEffect(() => {
+    setLoading(notFound ? <div className={classes.root}>Show not found</div> : <Loader />);
+    return () => '';
+  }, [show, notFound]);
+
+  return <div>{show ? <ShowPage show={show} /> : Loading}</div>;
 };
 
 ShowPageContainer.propTypes = {
